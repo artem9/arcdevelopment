69c10ab11719e1b3b6bc07810a87dc4dc50162a7
Artem Suzdal <artemsuzdal@gmail.com>
2022-12-10 17:12:48 +0200

    MUI ReactJS: Inputs, Validation and Confirmation - Using TextFields - State Controlled Values

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -6,11 +6,17 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import useTheme from '@mui/material/styles/useTheme';
 
 import { makeStyles } from 'tss-react/mui';
 
+import ButtonArrow from './ui/ButtonArrow';
+
 import background from '../assets/background.jpg';
+import phoneIcon from '../assets/phone.svg';
+import emailIcon from '../assets/email.svg';
+import airplane from '../assets/send.svg';
 
 const useStyles = makeStyles()((theme) => ({
   background: {
@@ -20,15 +26,47 @@ const useStyles = makeStyles()((theme) => ({
     backgroundRepeat: 'no-repeat',
     height: '60em',
   },
+  estimateButton: {
+    ...theme.typography.estimate,
+    borderRadius: 50,
+    height: 80,
+    width: 205,
+    backgroundColor: theme.palette.common.orange,
+    fontSize: '1.5rem',
+    marginRight: '5em',
+    marginLeft: '2em',
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.light,
+    },
+    [theme.breakpoints.down('md')]: {
+      marginLeft: 0,
+      marginRight: 0,
+    },
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: '0.7rem',
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down('md')]: {
+      marginBottom: '2em',
+    },
+  },
 }));
 
 function Contact({ setValue }) {
   const { classes } = useStyles();
   const theme = useTheme();
+  const matchesMD = useMediaQuery(theme.breakpoints.down('md'));
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
 
   return (
     <Grid container direction="row">
-      <Grid item container direction="column" lg={3}>
+      <Grid item container direction="column" justifyContent="center" lg={3}>
         <Grid item>
           <Typography variant="h2" style={{ lineHeight: 1 }}>
             Contact Us
@@ -40,8 +78,140 @@ function Contact({ setValue }) {
             We&apos;re waiting.
           </Typography>
         </Grid>
+        <Grid item container>
+          <Grid item>
+            <img
+              src={phoneIcon}
+              alt="phone"
+              style={{ marginRight: '0.5em', verticalAlign: 'bottom' }}
+            />
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              style={{ color: theme.palette.common.blue, fontSize: '1rem' }}
+            >
+              (555) 555-5555
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item container>
+          <Grid item>
+            <img
+              src={emailIcon}
+              alt="envelope"
+              style={{ marginRight: '0.5em', verticalAlign: 'bottom' }}
+            />
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              style={{ color: theme.palette.common.blue, fontSize: '1rem' }}
+            >
+              zachary@gmail.com
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item container>
+          <Grid item>
+            <TextField
+              label="Name"
+              id="name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              label="Email"
+              id="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              label="Phone"
+              id="phone"
+              value={phone}
+              onChange={(event) => setPhone(event.target.value)}
+            />
+          </Grid>
+        </Grid>
+        <Grid item>
+          <TextField
+            id="message"
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+            rows={10}
+            multiline
+          />
+        </Grid>
+        <Grid item>
+          <Button variant="contained">
+            Send Message <img src={airplane} alt="paper airplane" />
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item container className={classes.background} lg={9}/>
+      <Grid
+        item
+        container
+        className={classes.background}
+        alignItems="center"
+        lg={9}
+      >
+        <Grid
+          item
+          style={{
+            marginLeft: matchesMD ? 0 : '5em',
+            textAlign: matchesMD ? 'center' : 'inherit',
+          }}
+        >
+          <Grid container direction="column">
+            <Grid item>
+              <Typography variant="h2">
+                Simple Software.
+                <br />
+                Revolutionary Results.
+              </Typography>
+              <Typography variant="subtitle2" style={{ fontSize: '1.5rem' }}>
+                Take advantage of the 21st Century.
+              </Typography>
+              <Grid
+                container
+                item
+                justifyContent={matchesMD ? 'center' : undefined}
+              >
+                <Button
+                  variant="outlined"
+                  className={classes.learnButton}
+                  onClick={() => setValue(2)}
+                  component={Link}
+                  to="/revolution"
+                >
+                  <span style={{ marginRight: 5 }}>Learn More</span>
+                  <ButtonArrow
+                    width={10}
+                    height={10}
+                    fill={theme.palette.common.blue}
+                  />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            className={classes.estimateButton}
+            onClick={() => setValue(5)}
+            component={Link}
+            to="/estimate"
+          >
+            Free Estimate
+          </Button>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
